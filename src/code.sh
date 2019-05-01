974b1d6079055b778bf1c8ed3d21577d93f30793
woook <woook@users.noreply.github.com>
2019-05-01 07:51:14 +0100

    Remove gender specific reference creation workaround

--- src/code.sh
+++ src/code.sh
@@ -6,28 +6,14 @@ set -e -x -o pipefail
 function wcx_command {
 	# Call Wisecondor X to predict CNVs
 	# Args: (input.npz, reference.npz)
-    WisecondorX predict $1 $2 ${outdir}/${1%%.npz} --bed --plot\
+	WisecondorX predict $1 $2 ${outdir}/${1%%.npz} --bed --plot\
 		# --minrefbins $min_ref_bins
 		# --maskrepeats $mask_repeats
 		# --alpha $alpha
 		# --beta $beta
 		# --blacklist $blacklist
-		# --bed
-		# --plot
 }
 
-function wcx_run {
-	# Run WisecondorX dependent on input sample gender
-	# Args: input.npz
-	input_npz=$1
-	local gender=$(WisecondorX gender $input_npz)
-	# Run with appropriate reference for gender
-	if [[ $gender =~ "female" ]]; then
-		$(wcx_command $input_npz reference_female.npz)
-	elif [[ $gender =~ "male" ]]; then
-		$(wcx_command $input_npz reference_male.npz)
-	fi
-}
 
 # Download input data
 # project_for_newref -- project containing wisecondorx_reference directory with reference BAMs
@@ -44,55 +30,42 @@ bash Miniconda2-latest-Linux-x86_64.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 
 # Install WisecondorX
-conda install -f -y -c conda-forge -c bioconda wisecondorx
+# wisecondorx 1.1.0 is compatible with 1.16.2 but later versions of wisecondorx are compatible with newer numpy
+conda install -f -y -c conda-forge -c bioconda wisecondorx=1.1.0 numpy=1.16.2
 
 # Set the binsize for the `Wisecondor convert` command. 
 # All converted BAM files must use a binsize that is a multiple of the input resolution. Here we 
 # use a default of 1/20th.
 convert_binsize=$((resolution/20))
 
-###### Create reference files
+#### Create reference files
 
 # Download reference bams
 dx download -r ${project_for_newref}:/wisecondorx_reference/
 
-## delete self and downsampled self from reference bams - this depends entirely on first 4 characters of filename ## hack by Wook
+
+# delete self and downsampled self from reference bams - this depends entirely on first 4 characters of filename ## hack by Wook
 cd wisecondorx_reference
+ls
 bam_name=$input_bam_prefix
 bam_start=${bam_name:0:4}
 find . -maxdepth 1 -type f -name ${bam_start}\* -exec rm {} \;
 cd ..
 
+###WOUT
 # Convert all bams to numpy zip files
 for file in wisecondorx_reference/*.bam; do
 	prefix=${file%%.bam}
 	WisecondorX convert $file ${prefix}.npz --binsize $convert_binsize #--retdist $convert_retdist --retthres $convert_retthresh
 done
 
-# Create male and female sample holding directories
-male_dir="$HOME/male"
-female_dir="$HOME/female"
-mkdir -p $male_dir $female_dir
-
-# Separate the male and female reference sample npz files into respective directories
-# for file in wisecondorx_reference/*.npz; do
-# 	if [[ $(WisecondorX gender $file) =~ "male" ]]; then mv $file $male_dir 
-# 	elif [[ $(WisecondorX gender $file) =~ "female" ]]; then mv $file $female_dir
-#   	fi
-# done
-
-## QUICKFIX: Currently, available reference BAMs are all male. Use prefix input parameter to separate
-mv wisecondorx_reference/${reference_male_prefix}*.npz $male_dir
-mv wisecondorx_reference/${reference_female_prefix}*.npz $female_dir
-
-# Create references for Male and Female samples
-WisecondorX newref ${male_dir}/*.npz reference_male.npz --cpus 8 --binsize $resolution ## --refsize $ref_refsize
-WisecondorX newref ${female_dir}/*.npz reference_female.npz --cpus 8 --binsize $resolution ## --refsize $ref_refsizes
-#####
+###  Create reference
+WisecondorX newref wisecondorx_reference/*.npz wisecondorx_reference/combined_ref.npz --cpus 8 --binsize $resolution
+###WOUT END
 
 ##### Predict CNVs
 # Create output directory
-outdir=out/wisecondorx/Results/wisecondorx_${resolution}
+outdir=out/wisecondorx/wisecondorx_${resolution}/${input_bam_prefix}/
 mkdir -p $outdir
 
 # Convert input bam to numpy zip file for wisecondorx
@@ -100,8 +73,10 @@ mv $input_bam_index_path $(dirname $input_bam_path)
 WisecondorX convert $input_bam_path ${input_bam_prefix}.npz --binsize $convert_binsize #--retdist --retthres --gender --gonmapr
 
 # Run WisecondorX
-wcx_run ${input_bam_prefix}.npz
+wcx_command ${input_bam_prefix}.npz wisecondorx_reference/combined_ref.npz
 #####
 
 # Upload output data
+mv wisecondorx_reference/combined_ref.npz $outdir/combined_ref.npz
+mv ${input_bam_prefix}.npz $outdir/${input_bam_prefix}.npz
 dx-upload-all-outputs
